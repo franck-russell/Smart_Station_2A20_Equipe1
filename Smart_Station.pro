3942e80d4cffce3f66e3a7c7ea552d8b81820c38
Franck Russell <franckrussell.fongang@esprit.tn>
2021-10-22 10:28:13 +0100

    création du projet

--- Smart_Station.pro
+++ Smart_Station.pro
@@ -9,16 +9,16 @@ CONFIG += c++11
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 SOURCES += \
-    avions.cpp \
+    avion.cpp \
     main.cpp \
     mainwindow.cpp
 
 HEADERS += \
-    avions.h \
+    avion.h \
     mainwindow.h
 
 FORMS += \
-    avions.ui \
+    avion.ui \
     mainwindow.ui
 
 # Default rules for deployment.
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,29 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QPixmap>
+#include "avion.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QPixmap pix("D:/Esprit/2eme année/semestre 1/projet cpp/logo/image1");
+    ui->label_image1->setPixmap(pix);
+    //QPixmap pix("D:/Esprit/2eme année/semestre 1/projet cpp/logo/logo4");
+    //ui->label_logo4->setPixmap(pix);
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_pushButton_seconnecter_clicked()
+{
+//Avion a;
+//a.exec();
 }
 
