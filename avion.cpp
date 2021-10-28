c3e3c1912a2729b6dac28479d02bab532167f7af
Franck Russell <franckrussell.fongang@esprit.tn>
2021-10-28 18:50:57 +0100

    avant l'implémentation

--- avion.cpp
+++ avion.cpp
@@ -7,10 +7,10 @@ avion::avion(QWidget *parent) :
     ui(new Ui::avion)
 {
     ui->setupUi(this);
-    QPixmap pix("D:/Esprit/2eme année/semestre 1/projet cpp/logo/image2");
-    int w = ui->label_image2->width();
-    int h = ui->label_image2->height();
-    ui->label_image2->setPixmap(pix.scaled(w,h,Qt::KeepAspectRatio));
+    //QPixmap pix("D:/Esprit/2eme année/semestre 1/projet cpp/logo/image2");
+    //int w = ui->label_image2->width();
+    //int h = ui->label_image2->height();
+   // ui->label_image2->setPixmap(pix.scaled(w,h,Qt::KeepAspectRatio));
 }
 
 avion::~avion()
